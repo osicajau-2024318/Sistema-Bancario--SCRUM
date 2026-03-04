f3cf2fdc2af42258a3f6ff3ad97169d4c2f60f2c
jsajche-2024380 <jsajche-2024380@kinal.edu.gt>
2026-03-03 22:54:25 -0600

    Se actualizo el editar usuario como Admin y cliente

--- src/AuthServiceBanco.Application/DTOs/UpdateProfileDto.cs
+++ src/AuthServiceBanco.Application/DTOs/UpdateProfileDto.cs
@@ -4,6 +4,7 @@ public class UpdateProfileDto
 {
     public string? Name { get; set; }
     public string? Surname { get; set; }
+    public string? Phone { get; set; }
     public string? Address { get; set; }
     public string? WorkName { get; set; }
     public decimal? MonthlyIncome { get; set; }
