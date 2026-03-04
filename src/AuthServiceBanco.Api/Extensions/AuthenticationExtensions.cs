63c38250b7aa649d357deee0095829e26c56d09b
jsajche-2024380 <jsajche-2024380@kinal.edu.gt>
2026-03-03 23:33:03 -0600

    Se agrego los eventos de autenticacion al JwtBearer

--- src/AuthServiceBanco.Api/Extensions/AuthenticationExtensions.cs
+++ src/AuthServiceBanco.Api/Extensions/AuthenticationExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json;
 
 namespace AuthServiceBanco.Api.Extensions;
 
@@ -30,9 +31,63 @@ public static class AuthenticationExtensions
                 RoleClaimType = "role",
                 ClockSkew = TimeSpan.Zero
             };
+
+            // Eventos para devolver mensajes claros en lugar de 401/403 vacíos
+            options.Events = new JwtBearerEvents
+            {
+                OnChallenge = async context =>
+                {
+                    context.HandleResponse();
+
+                    string message;
+
+                    if (context.AuthenticateFailure is SecurityTokenExpiredException)
+                    {
+                        message = "El token ha expirado. Por favor inicia sesión nuevamente.";
+                    }
+                    else if (context.AuthenticateFailure is SecurityTokenInvalidSignatureException)
+                    {
+                        message = "Token inválido. La firma no es correcta.";
+                    }
+                    else if (context.AuthenticateFailure != null)
+                    {
+                        message = "Token inválido o malformado.";
+                    }
+                    else
+                    {
+                        message = "No se proporcionó token de autenticación.";
+                    }
+
+                    context.Response.StatusCode = 401;
+                    context.Response.ContentType = "application/json";
+
+                    var response = JsonSerializer.Serialize(new
+                    {
+                        success = false,
+                        message,
+                        errorCode = "UNAUTHORIZED"
+                    });
+
+                    await context.Response.WriteAsync(response);
+                },
+
+                OnForbidden = async context =>
+                {
+                    context.Response.StatusCode = 403;
+                    context.Response.ContentType = "application/json";
+
+                    var response = JsonSerializer.Serialize(new
+                    {
+                        success = false,
+                        message = "No tienes permisos para realizar esta acción.",
+                        errorCode = "FORBIDDEN"
+                    });
+
+                    await context.Response.WriteAsync(response);
+                }
+            };
         });
 
         return services;
     }
 }
-
